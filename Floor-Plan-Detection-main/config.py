ddd95318bf61c2d5498d0f846920634ff84112fe
saadkhan577 <gobudy3@gmail.com>
2025-02-28 10:31:07 -0800

    Update config.py

--- Floor-Plan-Detection-main/config.py
+++ Floor-Plan-Detection-main/config.py
@@ -1,12 +1,15 @@
 import os
 
-image_path = r"C:\Users\haier\Documents\clone Electrical\Floor-Plan-Detection-main\Images\example.png"
-target_path = r"C:\Users\haier\Documents\clone Electrical\Floor-Plan-Detection-main" # will export in two formats (.blend and .stl)
-program_path = os.getcwd()  
-blender_install_path = program_path+"\2.93.1\blender"
-blender_script_path = program_path+r"C:\Users\haier\Documents\clone Electrical\Floor-Plan-Detection-main\floorplan_to_3dObject_in_blender.py"
+# Define paths using os.path.join for platform independence
+image_path = os.path.join("C:", "Users", "haier", "Documents", "clone Electrical", "Floor-Plan-Detection-main", "Images", "example.png")
+target_path = os.path.join("C:", "Users", "haier", "Documents", "clone Electrical", "Floor-Plan-Detection-main")  # will export in two formats (.blend and .stl)
+program_path = os.getcwd()
+blender_install_path = os.path.join(program_path, "2.93.1", "blender")
+blender_script_path = os.path.join(program_path, "C:", "Users", "haier", "Documents", "clone Electrical", "Floor-Plan-Detection-main", "floorplan_to_3dObject_in_blender.py")
 
+# Super-Resolution settings
 SR_scale = 2
 SR_method = 'lapsrn'
 
+# CubiCasa setting
 CubiCasa = True
